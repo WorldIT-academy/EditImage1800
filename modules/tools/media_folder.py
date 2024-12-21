f6d2c3c8b533699ca3863effc099d01bcaa712c0
WorldIT-academy <worldit_academy@worldit.academy>
2024-12-21 19:36:08 +0200

    created modification of media_folder file

--- modules/tools/media_folder.py
+++ modules/tools/media_folder.py
@@ -1,13 +1,21 @@
 import os
+from os.path import abspath , join
+
 import colorama
 
 colorama.init()
 
 def create_folder_media():
-    if not os.path.exists('media'):  # check if directory exists
-        os.mkdir('media')
-        os.mkdir('media/images')
-        os.mkdir('media/images/downloads')
-        os.mkdir('media/images/edits')
+    try:
+        # creating path to media folder 
+        # ".." this command let move from file to folder or from folder to folder in reversal order 
+        # __file__ point start for creating abspath
+        path_media_folder = abspath(join(__file__, "..", "..", "..", "media"))
+
+        os.makedirs(join(path_media_folder, 'images', 'downloads'), exist_ok= True)
+        os.makedirs(join(path_media_folder, 'images', 'edits'), exist_ok= True)
+
         print(f"{colorama.Fore.GREEN}Creating folders{colorama.Style.RESET_ALL}")
+    except Exception as e:
+        print(f"{colorama.Fore.RED}Error: {str(e)}{colorama.Style.RESET_ALL}")
 
